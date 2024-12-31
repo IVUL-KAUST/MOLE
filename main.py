f8228222930a4d9eeb5f50f0424c2924077c077e
zaidalyafeai <alyafey22@gmail.com>
2025-01-01 00:53:13 +0300

    fix: update run endpoint to handle optional file uploads and process PDF content

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
-from fastapi import FastAPI, UploadFile, File
+from fastapi import FastAPI, UploadFile, File, Form
 from pydantic import BaseModel
 from pages.search import run
 
 app = FastAPI()
 
 @app.post("/run")
-async def func(link:str = '', file: UploadFile = File(...)):
-    pdf_content = file.file
-    
+async def func(link: str =  Form(None), file: UploadFile = File(None)):
+    if file != None:
+        pdf_content = file.file
+    else:
+        pdf_content = None
+
     model_name = 'claude-3-5-haiku-latest'
     # Call your processing function with the file content and link
     results = run(link = link, paper_pdf=pdf_content, models = model_name.split(','))
-    
+    print(results)
     return {'model_name': model_name, 'metadata': results[model_name]['metadata']}
     
