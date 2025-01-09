2ebf831089c2b0523008488aeef83f27bed6b79f
zaidalyafeai <alyafey22@gmail.com>
2025-01-09 14:12:58 +0300

    feat: add support for loading validation and test datasets from JSON files

--- constants.py
+++ constants.py
@@ -1,4 +1,6 @@
 from datasets import load_dataset
+from glob import glob
+
 MODEL_NAMES = ["gemini-1.5-flash", "gemini-1.5-pro", "claude-3-5-sonnet-latest", "claude-3-5-haiku-latest", "judge"]
 
 dialect_remapped = {'Classical Arabic': 'ar-CLS: (Arabic (Classic))','Modern Standard Arabic': 'ar-MSA: (Arabic (Modern Standard Arabic))','United Arab Emirates': 'ar-AE: (Arabic (United Arab Emirates))','Bahrain': 'ar-BH: (Arabic (Bahrain))','Djibouti': 'ar-DJ: (Arabic (Djibouti))','Algeria': 'ar-DZ: (Arabic (Algeria))','Egypt': 'ar-EG: (Arabic (Egypt))','Iraq': 'ar-IQ: (Arabic (Iraq))','Jordan': 'ar-JO: (Arabic (Jordan))','Comoros': 'ar-KM: (Arabic (Comoros))','Kuwait': 'ar-KW: (Arabic (Kuwait))','Lebanon': 'ar-LB: (Arabic (Lebanon))','Libya': 'ar-LY: (Arabic (Libya))','Morocco': 'ar-MA: (Arabic (Morocco))','Mauritania': 'ar-MR: (Arabic (Mauritania))','Oman': 'ar-OM: (Arabic (Oman))','Palestine': 'ar-PS: (Arabic (Palestine))','Qatar': 'ar-QA: (Arabic (Qatar))','Saudi Arabia': 'ar-SA: (Arabic (Saudi Arabia))','Sudan': 'ar-SD: (Arabic (Sudan))','Somalia': 'ar-SO: (Arabic (Somalia))','South Sudan': 'ar-SS: (Arabic (South Sudan))','Syria': 'ar-SY: (Arabic (Syria))','Tunisia': 'ar-TN: (Arabic (Tunisia))','Yemen': 'ar-YE: (Arabic (Yemen))','Levant': 'ar-LEV: (Arabic (Levant))','North Africa': 'ar-NOR: (Arabic (North Africa))','Gulf': 'ar-GLF: (Arabic (Gulf))','mixed': 'mixed'}
@@ -7,14 +9,14 @@ column_options = {
     'Dialect': ','.join(list(dialect_remapped.keys())),
     'Language': 'ar,multilingual',
     'Collection Style': 'crawling,annotation,machine translation,human translation,manual curation,LLM generated,other',
-    'Domain': 'social media,news articles,reviews,commentary,books,wikipedia,web pages,handwriting,LLM,other',
+    'Domain': 'social media,news articles,reviews,commentary,books,wikipedia,web pages,public datasets,TV Channels,captions,LLM,other',
     'Form': 'text,spoken,images',
     'Unit': 'tokens,sentences,documents,hours,images',
     'Ethical Risks': 'Low,Medium,High',
     'Script': 'Arab,Latin,Arab-Latin',
     'Tokenized': 'Yes,No',
     'Host': 'CAMeL Resources,CodaLab,data.world,Dropbox,Gdrive,GitHub,GitLab,kaggle,LDC,MPDI,Mendeley Data,Mozilla,OneDrive,QCRI Resources,ResearchGate,sourceforge,zenodo,HuggingFace,ELRA,other',
-    'Access': 'Free,Upon-Request,Paid',
+    'Access': 'Free,Upon-Request,With-Fee',
     'Test Split': 'Yes,No',
     'Tasks': 'machine translation,speech recognition,sentiment analysis,language modeling,topic classification,dialect identification,text generation,cross-lingual information retrieval,named entity recognition,question answering,information retrieval,part of speech tagging,language identification,summarization,speaker identification,transliteration,morphological analysis,offensive language detection,review classification,gender identification,fake news detection,dependency parsing,irony detection,meter classification,natural language inference,instruction tuning,other',
     'Venue Type': 'conference,workshop,journal,preprint'
@@ -69,4 +71,9 @@ system_prompt = "You are a profressional research paper reader. You will be prov
             If the question has no choices and the answer is not found in the paper, then answer empty string. \
             Each question is in the format Key:question, please use Key as a json key for each question and return the answer in json"
 
-masader_dataset = load_dataset('arbml/masader')['train']
+masader_dataset = load_dataset('arbml/masader', keep_in_memory=True)['train']
+masader_valid_dataset = load_dataset('json', data_files = glob('validset/**.json'), keep_in_memory=True)['train']
+masader_test_dataset = load_dataset('json', data_files = glob('testset/**.json'), keep_in_memory=True)['train']
+
+TEST_DATASETS_IDS = ['1709.07276', '1610.00572', '2404.00565', '2201.06723', '2402.07448', '2210.12985', '2106.10745', '1812.10464', '1910.07475', '2004.06465', '2103.09687', '2004.14303', '2005.06608', '1808.07674', '2106.03193', '1612.08989', '1610.09565', '1809.03891', '1402.0578', '1410.3791', '1910.10683', '1907.03110', '2407.19835', '2010.11856', '1809.05053']
+VALID_DATASETS_IDS = ['1609.05625', '2402.03177', '2405.01590', '2402.12840', '1906.00591']
