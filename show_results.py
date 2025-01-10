25509a828d0a420f868dba48ea4d875019cdb59e
zaidalyafeai <alyafey22@gmail.com>
2025-01-10 23:13:43 +0300

    remove unused import for get_arxiv_id in show_results.py

--- show_results.py
+++ show_results.py
@@ -4,7 +4,6 @@ import json
 import numpy as np
 import argparse
 from constants import *
-from utils import get_arxiv_id
 
 args = argparse.ArgumentParser()
 args.add_argument('--eval', type=str, default='valid')
