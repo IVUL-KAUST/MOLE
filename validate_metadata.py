5cf1d918d4cf2663b35018552e9afc1dc597620f
zaidalyafeai <alyafey22@gmail.com>
2025-04-24 09:48:36 +0300

    fix metadata annotations

---
+++ validate_metadata.py
@@ -0,0 +1,15 @@
+import json
+import glob
+import os
+for schema in os.listdir('schema'):
+    lang = schema.split('.')[0]
+    metadata = json.load(open(f'schema/{schema}'))
+    for file in glob.glob(f'evals/{lang}/**/*.json'):
+        new_metadata = json.load(open(file))
+        for key in metadata:
+            if key not in new_metadata:
+                print(f"{file} is missing {key}")
+        annotations_from_paper = new_metadata["annotations_from_paper"]
+        for key in metadata:
+            if key not in annotations_from_paper:
+                print(f"{file} is missing {key} in annotations_from_paper")
