070502b3edb6f1319032509d866e43a671e32f89
zaidalyafeai <alyafey22@gmail.com>
2025-06-27 16:58:23 +0300

    add emotion classification task

--- main.py
+++ main.py
@@ -12,9 +12,10 @@ async def func(link: str =  Form(''), schema: str = Form(''), file: UploadFile =
         pdf_content = None
 
     browse_web = False
-    model_name = 'google/gemma-3-27b-it'
+    model_name = 'google/gemini-2.5-pro'
+
     # Call your processing function with the file content and link
-    results = run(link = link, paper_pdf=pdf_content, models = model_name.split(','), overwrite=True, few_shot = 0, schema = schema)
+    results = run(link = link, paper_pdf=pdf_content, models = model_name.split(','), overwrite=False, few_shot = 0, schema = schema, pdf_mode = 'plumber')
     
     # print(results)
     # results = json.load(open('/Users/zaidalyafeai/Documents/Development/masader_bot/static/results_latex/1410.3791/zero_shot/google_gemma-3-27b-it-browsing-results.json'))
