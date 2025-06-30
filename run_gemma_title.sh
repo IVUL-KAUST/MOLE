356aad081899f77c4348fb0559c7dae5391fd992
zaidalyafeai <alyafey22@gmail.com>
2025-06-30 15:36:22 +0300

    add title generation scripts

--- run_gemma_title.sh
+++ run_gemma_title.sh
@@ -1,4 +1,4 @@
 for lang in ar en fr ru jp multi
 do
-    python evaluate.py --models google/gemma-3-27b-it -mt --schema $lang --few_shot 0 --results_path results_title --use_title
+    python evaluate.py --models google/gemma-3-27b-it -mt --schema $lang --few_shot 0 --results_path results_title --use_title &
 done
