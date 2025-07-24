2c37985fc18a0942b6f49a8f3838aa20aa334824
zaidalyafeai <alyafey22@gamil.com>
2025-07-25 00:09:39 +0300

    change the path of training scripts

--- src/training/create_train_dataset.py
+++ src/training/create_train_dataset.py
@@ -1,22 +1,28 @@
-from src.search_acl import annotate_schema
 import pandas as pd
 import time
 import concurrent.futures
 import os
+from glob import glob
+
 dfs = []
 manual_annotation = False
+base_dir = "../.cache/jql-**/"
 if manual_annotation:
     papers = annotate_schema()
     df = pd.DataFrame(papers)
 else:
-    results = os.listdir('../.cache/jql-resources')[0]
-    path = f'../.cache/jql-resources/{results}/results.jsonl'
-    df = pd.read_json(path, lines=True)
+    dfs = []
+    for file in glob(base_dir + '*/results.jsonl'):
+        df = pd.read_json(file, lines=True)
+        dfs.append(df)
+    df = pd.concat(dfs)
     df.rename(columns={'category': 'schema_name'}, inplace=True)
     print(df)
+    print(df['schema_name'].value_counts())
     
 
-df = df[df['schema_name'] != '']
+acceptable_schemas = ['ar', 'en', 'ru', 'jp', 'fr', 'multi', 'other']
+df = df[df['schema_name'].isin(acceptable_schemas)]
 
 # choose schema randomly according to the schdema_name with the lower number of papers
 # Determine the minimum group size
@@ -25,14 +31,14 @@ print(df['schema_name'].value_counts())
 min_group_size = df.groupby('schema_name').size().min()
 # Sample the same number of elements from each category
 
-sampled_df = df[df['schema_name'] != 'ru'].groupby('schema_name').sample(n=150, random_state=42, replace=False) # random_state for reproducibility
+sampled_df = df[df['schema_name'] != 'ru'].groupby('schema_name').sample(n=400, random_state=42, replace=False) # random_state for reproducibility
 df = pd.concat([sampled_df, df[df['schema_name'] == 'ru']], axis = 0)
 # plot the distribution of the schema_name
 print(df['schema_name'].value_counts())
 
 # filter 
 print('filtering the test dataset')
-test_dataset = pd.read_csv('test_dataset.csv')
+test_dataset = pd.read_csv('src/classification/test_dataset.csv')
 train_df = df[~df['title'].isin(test_dataset['title'])]
 
 # print(train_df["schema_name"].value_counts())
