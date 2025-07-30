1e2e1c0c51697d9854b5651a2cd8a9e2b766c4f7
zaidalyafeai <alyafey22@gamil.com>
2025-07-30 15:49:45 +0300

    update dataset pushing

--- src/push_mole_resources.py
+++ src/push_mole_resources.py
@@ -2,19 +2,20 @@ from datasets import load_dataset, DatasetDict, concatenate_datasets
 
 train_dataset = load_dataset('csv', data_files='data/train_dataset.csv', split='train')
 test_dataset = load_dataset('csv', data_files='data/test_dataset.csv', split='train')
-test_dataset = test_dataset.add_column('reasoning', ['']*len(test_dataset))
+test_dataset = test_dataset.add_column('reasoning', ['human annotated']*len(test_dataset))
 
 dataset = DatasetDict({
     'train': train_dataset,
     'test': test_dataset
 })
 
+print("mole resources")
 print(dataset)
-# dataset.push_to_hub('IVUL-KAUST/mole-resources', private=True)
-papers_arxiv = load_dataset('csv', data_files='data/arxiv_papers.csv', split='train')
-papers_acl = load_dataset('csv', data_files='data/acl_papers.csv', split='train')
+dataset.push_to_hub('IVUL-KAUST/mole-resources', private=True)
+papers_arxiv = load_dataset('csv', data_files='data/all_papers.csv', split='train')
 
-concatenated_dataset = concatenate_datasets([papers_arxiv, papers_acl])
-
+concatenated_dataset = concatenate_datasets([papers_arxiv])
+concatenated_dataset = concatenated_dataset.remove_columns(['id'])
+print("mole papers")
 print(concatenated_dataset)
-# concatenated_dataset.push_to_hub('IVUL-KAUST/mole-papers', private=True)
+concatenated_dataset.push_to_hub('IVUL-KAUST/mole-papers', private=True)
