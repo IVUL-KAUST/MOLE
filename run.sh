e66445ce3980d80ed94772e42b37a439fa9502f3
zaidalyafeai <alyafey22@gamil.com>
2025-08-03 00:41:58 +0300

    update scripts

--- run.sh
+++ run.sh
@@ -1,1 +1,3 @@
-python src/evaluate.py --model gemma-3-4b-it-sft --backend vllm --split test --max_model_len 8912 --max_output_len 2048 --overwrite
+for schema in ar en fr ru jp multi; do
+    python src/evaluate.py --model gemma-3-4b-it-sft-4096 --backend vllm --split test --schema_name $schema --max_model_len 4096 --max_output_len 2048 --overwrite
+done
--- run_gemma4b.sh
+++ run_gemma4b.sh
@@ -1,1 +1,3 @@
-python src/evaluate.py --model google/gemma-3-4b-it --backend openrouter --split valid --overwrite
+for schema in ar en fr ru jp multi; do
+    python src/evaluate.py --model google/gemma-3-4b-it --backend openrouter --split test --schema_name $schema
+done
