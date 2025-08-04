4a942b683c2226451fec0ae4c4d51d8b9c84ee51
zaidalyafeai <alyafey22@gamil.com>
2025-08-04 22:30:42 +0300

    use load datasets to speed up filtering and the levenshtein distance to filter out the test dataset.

--- src/training/create_train_dataset.py
+++ src/training/create_train_dataset.py
@@ -3,6 +3,8 @@ import time
 import concurrent.futures
 import os
 from glob import glob
+from tqdm import tqdm
+from collections import Counter
 
 dfs = []
 manual_annotation = False
@@ -19,30 +21,56 @@ else:
     df.rename(columns={'category': 'schema_name'}, inplace=True)
     df.drop(columns= ['id'], inplace=True)
     
+    df['title'] = df['title'].str.replace('\"', '')
+    print("before drop duplicates")
+    print(df.shape[0])
+    # drop duplicates
+    df.drop_duplicates(subset=['title'], keep='first', inplace=True)
+    print("after drop duplicates")
+    print(df.shape[0])
+
+from datasets import Dataset, load_dataset
+dataset = Dataset.from_pandas(df, preserve_index=False)
 
 acceptable_schemas = ['ar', 'en', 'ru', 'jp', 'fr', 'multi', 'other', 'none']
-df = df[df['schema_name'].isin(acceptable_schemas)]
+dataset = dataset.filter(lambda x: x['schema_name'] in acceptable_schemas)
+print(dataset)
 
-# choose schema randomly according to the schdema_name with the lower number of papers
-# Determine the minimum group size
-print(df['schema_name'].value_counts())
+print(Counter(dataset["schema_name"]))
 
-df.to_csv('data/all_papers.csv', index=False, encoding='utf-8')
+dataset.to_csv('data/all_papers.csv', index=False, encoding='utf-8')
+
+print('filtering the test dataset')
+test_dataset = load_dataset('csv', data_files='data/test_dataset.csv', split='train')
+
+# use lavenstein distance to filter out the test dataset
+from Levenshtein import distance
+def filter_by_distance(examples):
+    output = []
+    for title in examples['title']:
+        for test_title in test_dataset['title']:
+            d = distance(title.lower(), test_title.lower())
+            if d <= 5:
+                output.append(False)
+                break
+        else:
+            output.append(True)
+    return output
+dataset = dataset.filter(filter_by_distance, batched=True, num_proc=4)
+print(Counter(dataset["schema_name"]))
+
+df = dataset.to_pandas()
+print(df["schema_name"].value_counts())
 
 min_group_size = df.groupby('schema_name').size().min()
 # Sample the same number of elements from each category
 
-sampled_df = df[df['schema_name'] != 'ru'].groupby('schema_name').sample(n=400, random_state=42, replace=False) # random_state for reproducibility
-df = pd.concat([sampled_df, df[df['schema_name'] == 'ru']], axis = 0)
+sampled_df = df[df['schema_name'] != 'ru'].groupby('schema_name').sample(n=350, random_state=42, replace=False) # random_state for reproducibility
+train_df = pd.concat([sampled_df, df[df['schema_name'] == 'ru']], axis = 0)
 # plot the distribution of the schema_name
-print(df['schema_name'].value_counts())
 
-# filter 
-print('filtering the test dataset')
-test_dataset = pd.read_csv('data/test_dataset.csv')
-train_df = df[~df['title'].isin(test_dataset['title'])]
 
-# print(train_df["schema_name"].value_counts())
+print(train_df["schema_name"].value_counts())
 
 
 print(train_df['schema_name'].value_counts())
