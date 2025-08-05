37071bcbef84ea2390cf0f4338e9d691789c2135
zaidalyafeai <alyafey22@gamil.com>
2025-08-05 22:52:42 +0300

    use datasets map to speed up synth dataset creation

--- src/create_synth_dataset.py
+++ src/create_synth_dataset.py
@@ -5,7 +5,14 @@ import time
 import sys
 from openai import OpenAI
 from tqdm import tqdm
-import concurrent.futures   
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--model_name", type=str, required=True)
+parser.add_argument("--batch_size", type=int, required=False, default=6)
+parser.add_argument("--num_proc", type=int, required=False, default=6)
+args = parser.parse_args()
+
 dataset = load_dataset('csv', data_files='train_dataset.csv', split='train')
 dataset = dataset.filter(lambda x: x['schema_name'] in ['ar', 'ru', 'en', 'jp', 'fr', 'multi'])
 def check_server_status(model, HOST = "localhost", PORT = 8787):
@@ -24,25 +31,29 @@ def check_server_status(model, HOST = "localhost", PORT = 8787):
                 ]
             )
             print("inference done")
-            print(chat_response)
             return True
         except Exception as e:
             print(f"Error: {e}")
             return False
 
+def inference(examples):
+    urls = examples['url']
+    schema_names = examples['schema_name']
+    for url, schema_name in zip(urls, schema_names):
+        try:
+            run(url, model_name=args.model_name, schema_name=schema_name, backend='vllm', results_path='synth_datasetv2', format='pdf_plumber', max_model_len=32768, max_output_len=2048, log = False)
+        except Exception as e:
+            continue
+    return examples
+
 if __name__ == "__main__":
-    model_name = sys.argv[1]
-    while not check_server_status(model_name):
+    while not check_server_status(args.model_name):
         print("Server is not ready, waiting for 1 second")
         time.sleep(1)
     print("Server is ready")
 
     # I need to speed up the process using threads (50 threads at maximum)
-    with concurrent.futures.ThreadPoolExecutor(max_workers= 6) as executor:
-        for example in tqdm(dataset):
-            url = example['url']
-            schema_name = example['schema_name']
-            executor.submit(run, url, model_name=model_name, schema_name=schema_name, backend='vllm', results_path='synth_datasetv2', format='pdf_plumber', max_model_len=32768, max_output_len=2048)
+    dataset.map(inference, batched=True, batch_size=args.batch_size, num_proc=args.num_proc)
         
 
 
