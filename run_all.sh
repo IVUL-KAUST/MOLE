5493791cf85abdb5f9db2324e5a392a797e4a192
zaidalyafeai <alyafey22@gamil.com>
2025-08-25 13:16:28 +0300

    update scripts to use uv run

---
+++ run_all.sh
@@ -0,0 +1,7 @@
+bash run_gemma.sh
+bash run_claude.sh
+bash run_deepseek.sh
+bash run_gemini.sh
+bash run_gpt.sh
+bash run_llama.sh
+bash run_qwen.sh
--- run_qwen.sh
+++ run_qwen.sh
@@ -1,4 +1,4 @@
 for lang in ar en fr ru jp multi
 do
-    python evaluate.py --models qwen/qwen-2.5-72b-instruct -mt --schema $lang --few_shot 0 --results_path results_latex &
+    uv run evaluate.py --models qwen/qwen-2.5-72b-instruct -mt --schema $lang --few_shot 0 -b --results_path results_latex &
 done
