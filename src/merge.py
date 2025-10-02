36a904e2e0787a5c255a4cb274e3a19baf59e9dc
zaidalyafeai <alyafey22@gamil.com>
2025-10-02 22:58:54 +0300

    code for merging

--- src/merge.py
+++ src/merge.py
@@ -1,15 +1,23 @@
 from peft import PeftModel
 from transformers import AutoModelForCausalLM
 import os
-base_model = "Qwen2.5-3B-Instruct-kimi-k2-sft-8192"
-lora_model = "Qwen2.5-3B-Instruct-kimi-k2-dpo-8192-adapters/checkpoint-100"
-save_model = "Qwen2.5-3B-Instruct-kimi-k2-dpo-merged-100"
-base = AutoModelForCausalLM.from_pretrained(base_model)
-lora = PeftModel.from_pretrained(base, lora_model)
+import argparse
 
+parser = argparse.ArgumentParser()
+parser.add_argument("--base_model", type=str, required=True)
+parser.add_argument("--lora_model", type=str, required=True)
+parser.add_argument("--save_model", type=str, required=True)
+args = parser.parse_args()
+
+base = AutoModelForCausalLM.from_pretrained(args.base_model)
+lora = PeftModel.from_pretrained(base, args.lora_model)
+
+print("Saving merged models")
 # merge adapter weights into base model
 merged = lora.merge_and_unload()
-merged.save_pretrained(save_model)
+merged.save_pretrained(args.save_model)
 
-os.system(f"cp {base_model}/tokenizer.json {base_model}/vocab.json {base_model}/merges.txt {base_model}/added_tokens.json {save_model}/")
-os.system(f"wget https://huggingface.co/Qwen/Qwen2.5-7B-Instruct/raw/main/tokenizer_config.json -O {save_model}/tokenizer_config.json")
+print('Copying tokenizer files')
+print(args.base_model)
+os.system(f"cp {args.base_model}/tokenizer.json {args.base_model}/vocab.json {args.base_model}/merges.txt {args.base_model}/added_tokens.json {args.save_model}/")
+os.system(f"wget https://huggingface.co/Qwen/Qwen2.5-7B-Instruct/raw/main/tokenizer_config.json -O {args.save_model}/tokenizer_config.json")
