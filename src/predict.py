2cc312bf7d67a46950010cfc44b3848dcb64ec58
zaidalyafeai <alyafey22@gamil.com>
2025-11-19 11:59:20 +0300

    add predict

--- src/predict.py
+++ src/predict.py
@@ -1,9 +1,11 @@
 from search import get_metadata
 from rich import print
+from schema import TextSchema
+from type_classes import *
 
-def extract(text, model_name, schema_name, backend = "openrouter", max_model_len = 8192, max_output_len = 2084):
+def extract(text, model_name, schema_name = "ar", backend = "openrouter", max_model_len = 8192, max_output_len = 2084, schema = None):
     message, metadata, cost, error = get_metadata(
-        text, model_name, schema_name=schema_name, backend = backend, log = False, max_model_len = max_model_len, max_output_len = max_output_len
+        text, model_name, schema_name=schema_name, backend = backend, log = False, max_model_len = max_model_len, max_output_len = max_output_len, schema = schema
     )
     return metadata
 
@@ -13,12 +15,28 @@ benchmark and achieved impressive results. The arhitecture is based on Transform
 under the Apache 2.0 license. The model can process up to 8192 tokens. The model was released in 2025 from KAUST.
 """
 
+# metadata = extract(
+#     text, "Qwen2.5-3B-Instruct-kimi-k2-sft-8192", schema_name="model", backend = "vllm"   
+# )
+# print(metadata)
+
+# metadata = extract(
+#     text, "moonshotai/kimi-k2", schema_name="model", backend = "openrouter"   
+# )
+# print(metadata)
+
+class ExampleSchema(TextSchema):
+    Name: Field(Str, 1, 5)
+    Hobbies: Field(List[Str], 1, 1, ['Hiking', 'Swimming', 'Reading'])
+    Age : Field(Int, 1, 100)
+    Married: Field(Bool, 1, 1)
+
+text = """
+My name is Zaid. I am 25 years old. I like swimming and reading. I am is married. 
+"""
 metadata = extract(
-    text, "Qwen2.5-3B-Instruct-kimi-k2-sft-8192", schema_name="model", backend = "vllm"   
+    text, "IVUL-KAUST/MeXtract-3B", schema=ExampleSchema, backend = "transformers"   
 )
 print(metadata)
 
-metadata = extract(
-    text, "moonshotai/kimi-k2", schema_name="model", backend = "openrouter"   
-)
-print(metadata)
+
